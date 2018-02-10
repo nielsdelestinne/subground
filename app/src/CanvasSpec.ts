89fae66a5349b3151a66e6fd5a7720cc41e42525
nielsdelestinne <nielsdelestinne@gmail.com>
2018-02-10 19:08:46 +0100

    Canvas default width & height increased

--- app/src/CanvasSpec.ts
+++ app/src/CanvasSpec.ts
@@ -1,6 +1,6 @@
 export default class CanvasSpec {
     public static readonly CANVAS_ID: string = "subgroundCanvas";
-    public static readonly CANVAS_WIDTH: number = 10;
-    public static readonly CANVAS_HEIGHT: number = 50;
+    public static readonly CANVAS_WIDTH: number = 500;
+    public static readonly CANVAS_HEIGHT: number = 400;
     public static readonly CANVAS_BACKGROUND_COLOR: string = "#ff3";
 }
