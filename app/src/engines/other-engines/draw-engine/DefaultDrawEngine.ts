ab3b06a3be5e719ffcfde7ad534136c697ffd792
nielsdelestinne <nielsdelestinne@gmail.com>
2018-03-17 15:00:51 +0100

    drawables added to draw engine

--- app/src/engines/other-engines/draw-engine/DefaultDrawEngine.ts
+++ app/src/engines/other-engines/draw-engine/DefaultDrawEngine.ts
@@ -1,24 +1,35 @@
 import CanvasSpec from "../../../CanvasSpec";
 import Engine from "../Engine";
+import Drawable from "./drawable-domain/Drawable";
 
 export default class DrawEngine implements Engine {
 
     private canvasElement: any;
-    private canvasContext: any;
 
-    constructor() {
+    private canvasContext: any;
+    constructor(private drawables: Drawable[]) {
         this.canvasElement = DrawEngine.createCanvas();
         this.canvasContext = this.canvasElement.getContext("2d");
     }
 
+    public addDrawables(drawables: Drawable[]) {
+        this.drawables.push(...drawables);
+    }
+
     public execute(): void{
         this.drawBackground();
+        this.drawDrawables();
     }
 
     private drawBackground(): void {
         this.canvasContext.rect(0, 0, CanvasSpec.CANVAS_WIDTH, CanvasSpec.CANVAS_HEIGHT);
         this.canvasContext.fillStyle = CanvasSpec.CANVAS_BACKGROUND_COLOR;
         this.canvasContext.fill();
+    }
+
+    drawDrawables(): void {
+        this.drawables
+            .forEach(drawable => drawable.draw(this.canvasContext));
     }
 
     private static createCanvas(): HTMLElement{
