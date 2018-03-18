1755c0b758d261dae39fe9e4fd46568d068352a1
nielsdelestinne <nielsdelestinne@gmail.com>
2018-03-18 15:41:45 +0100

    Same height as width

--- app/src/CanvasSpec.ts
+++ app/src/CanvasSpec.ts
@@ -6,6 +6,6 @@
 export default class CanvasSpec {
     public static readonly CANVAS_ID: string = "subgroundCanvas";
     public static readonly CANVAS_WIDTH: number = document.getElementById("gui").offsetWidth;
-    public static readonly CANVAS_HEIGHT: number = 800;
+    public static readonly CANVAS_HEIGHT: number = document.getElementById("gui").offsetWidth; // same height as width
     public static readonly CANVAS_BACKGROUND_COLOR: string = "#181818";
 }
